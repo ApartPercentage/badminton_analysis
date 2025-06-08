b5778327ad68b52d1a32e60d430cbcc02bf7f9ca
Yu Chen Lee <chen@coefficient.ai>
2025-06-08 01:59:55 +0100

    CORS issue

--- badminton_analysis/settings.py
+++ badminton_analysis/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+if not ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', 'badminton-analysis-dy98.onrender.com',  # Your backend domain
+    'badminton-analysis-frontend.onrender.com', ]
 
 
 # Application definition
